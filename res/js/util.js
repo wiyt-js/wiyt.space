5dc719269be8de29626df3b8d9091d3276a3508d
mishyVT <white.skidmore@gmail.com>
2025-09-01 22:31:38 -0600

    Dark/light modes using 'localStorage'

--- res/js/util.js
+++ res/js/util.js
@@ -10,3 +10,38 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Current year
 document.getElementById('year').innerHTML = new Date().getFullYear();
+
+// Theme
+const themeToggle = document.getElementById('theme-toggle'),
+    mainElement = document.documentElement,
+    savedTheme = localStorage.getItem('theme'),
+    hoverTitle = document.querySelector('a#theme-toggle i[title="Theme"]');
+function toggleIcon(isDark) {
+    if(isDark) {
+        hoverTitle.classList.remove('fa-moon');
+        hoverTitle.classList.add('fa-sun');
+    } else {
+        hoverTitle.classList.remove('fa-sun');
+        hoverTitle.classList.add('fa-moon');
+    }
+}
+addEventListener("DOMContentLoaded", (event) => {
+    if(savedTheme) {
+        const isLight = savedTheme === 'light';
+        mainElement.setAttribute('data-theme', savedTheme);
+        toggleIcon(!isLight);
+        hoverTitle.title = (isLight ? 'Dark mode' : 'Light mode');
+    } else {
+        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        mainElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
+        toggleIcon(prefersDark);
+    }
+    themeToggle.addEventListener('click', () => {
+        const theme = mainElement.getAttribute('data-theme'),
+            newTheme = theme === 'light' ? 'dark' : 'light';
+        mainElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+        toggleIcon(theme === 'light');
+        hoverTitle.title = (theme.charAt(0).toUpperCase() + theme.slice(1)) + " mode";
+    });
+});
