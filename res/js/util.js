2af6693d0b5f54053860288145ca5452ad69bfbb
mishyVT <white.skidmore@gmail.com>
2025-09-03 12:12:55 -0600

    Reorganized 'util.js'

--- res/js/util.js
+++ res/js/util.js
@@ -1,3 +1,44 @@
+// Theme
+const themeToggle = document.getElementById('theme-toggle'),
+    mainElement = document.documentElement,
+    savedTheme = localStorage.getItem('theme'),
+    hoverTitle = document.querySelector('a#theme-toggle i[title="Theme"]');
+
+function toggleIcon(isDark) {
+    if(isDark) {
+        hoverTitle.classList.add('fa-sun');
+        hoverTitle.classList.remove('fa-moon');
+    } else {
+        hoverTitle.classList.add('fa-moon');
+        hoverTitle.classList.remove('fa-sun');
+    }
+}
+
+function setTheme(theme) {
+    mainElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+if(savedTheme) {
+    const isLight = savedTheme === 'light';
+    setTheme(savedTheme);
+    toggleIcon(!isLight);
+    hoverTitle.title = (isLight ? 'Dark mode' : 'Light mode');
+} else {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setTheme(prefersDark ? 'dark' : 'light');
+    toggleIcon(prefersDark);
+    hoverTitle.title = (!prefersDark ? 'Dark' : 'Light') + " mode";
+}
+
+themeToggle.addEventListener('click', () => {
+    const theme = mainElement.getAttribute('data-theme'),
+        newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    toggleIcon(theme === 'light');
+    hoverTitle.title = (theme.charAt(0).toUpperCase() + theme.slice(1)) + " mode";
+});
+
 // Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function(event) {
@@ -10,38 +51,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Current year
 document.getElementById('year').innerHTML = new Date().getFullYear();
-
-// Theme
-const themeToggle = document.getElementById('theme-toggle'),
-    mainElement = document.documentElement,
-    savedTheme = localStorage.getItem('theme'),
-    hoverTitle = document.querySelector('a#theme-toggle i[title="Theme"]');
-function toggleIcon(isDark) {
-    if(isDark) {
-        hoverTitle.classList.remove('fa-moon');
-        hoverTitle.classList.add('fa-sun');
-    } else {
-        hoverTitle.classList.remove('fa-sun');
-        hoverTitle.classList.add('fa-moon');
-    }
-}
-addEventListener("DOMContentLoaded", (event) => {
-    if(savedTheme) {
-        const isLight = savedTheme === 'light';
-        mainElement.setAttribute('data-theme', savedTheme);
-        toggleIcon(!isLight);
-        hoverTitle.title = (isLight ? 'Dark mode' : 'Light mode');
-    } else {
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        mainElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
-        toggleIcon(prefersDark);
-    }
-    themeToggle.addEventListener('click', () => {
-        const theme = mainElement.getAttribute('data-theme'),
-            newTheme = theme === 'light' ? 'dark' : 'light';
-        mainElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-        toggleIcon(theme === 'light');
-        hoverTitle.title = (theme.charAt(0).toUpperCase() + theme.slice(1)) + " mode";
-    });
-});
